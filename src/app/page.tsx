ee3a8134f3a36c7c76673ba9dd4a43b5826be6bd
jcalvacheoland <jcalvache@olandseguros.com>
2025-10-11 16:13:28 -0500

    add: la apliacion siempre inica en /login. El archivo page home, tiene un redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-   <div className="text-center mt-20">
-    <h1>Pagina principal</h1>
-    <h2 className="mt-12">Click en el siguiente boton para ir a la pagina de Inicio de sesion</h2>
-    <Button >
-      <Link href="/login">
-      </Link>
-    </Button>
-
-   </div>
-  );
+  redirect("/login");
 }
