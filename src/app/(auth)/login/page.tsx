1e8ab948723f95120fd15d7adb35ab4f0a374277
jcalvacheoland <jcalvache@olandseguros.com>
2025-10-17 18:14:33 -0500

    fixed: manejo de sesión y conexión Prisma — unificación de instancia global, redirección de login a dashboard para usuarios autenticados, y protección básica de rutas con Auth.js

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,19 @@
 import { LoginCard } from "./components/LoginCard";
+import { auth } from "@/auth" // si usas la configuración de Auth.js 5+
+import { redirect } from "next/navigation"
 
-export default function LoginPage() {
-  return <LoginCard />;
+export default async function LoginPage() {
+
+  const session = await auth() // 🔹 obtiene la sesión actual
+
+  if (session) {
+    // 🔹 Si el usuario ya está autenticado
+    redirect("/dashboard")
+  }
+
+  return(
+    <div>
+      <LoginCard />;
+    </div>
+  ) 
 }
