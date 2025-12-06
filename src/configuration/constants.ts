251ea9b83501ca4e54f983a8423f03020ff95ef2
jcalvacheoland <jcalvache@olandseguros.com>
2025-12-05 19:31:44 -0500

    delete: elimine standalone del next.config.ts

--- src/configuration/constants.ts
+++ src/configuration/constants.ts
@@ -6,9 +6,10 @@ export const BITRIX_WEBHOOK=process.env.BITRIX_WEBHOOK;
 export const HOST_CATALOG = process.env.HOST_CATALOG;
 export const CLIENT_SECRET = process.env.CLIENT_SECRET;
 export const HOST_TOKEN= process.env.HOST_TOKEN;
+//equisuiza
 export const OCP_APIM_KEY = process.env.OCP_APIM_KEY; 
 
-export const Aseguradoras = ["zurich", "chubb","sweaden", "aig", ]; 
+export const Aseguradoras = ["equisuiza","zurich", "chubb","sweaden", "aig", ]; 
 
 export const ECUADOR_CITIES = [
   "QUITO",
